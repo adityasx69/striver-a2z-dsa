8a4618a54d0da0b7db50bd8f70d341eac8d91929
Aditya Saraswat <saraswataditya664@gmail.com>
2025-10-06 19:31:44 +0530

    add ReverseANumber.java

--- 02_BasicMaths/ReverseANumber.java
+++ 02_BasicMaths/ReverseANumber.java
@@ -5,15 +5,28 @@ public class ReverseANumber {
 	public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        
+        int temp = n;
         int revNo = 0;
         int lastdigit;
+
+        int sign;
+        if (n<0){
+            sign = -1;
+        }
+        else{
+            sign = 1;
+        }
+
+        n = Math.abs(n);
 
         while(n>0){
             lastdigit = n % 10;
             n = n / 10;
             revNo = (revNo * 10) + lastdigit;
         }
+
+        revNo = revNo * sign;
+
         System.out.println(revNo);
 	}
 }
