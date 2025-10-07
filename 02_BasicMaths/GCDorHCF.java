2e371f0c2f550d5d1f16a5ebc5d7f817c15154fc
Aditya Saraswat <saraswataditya664@gmail.com>
2025-10-07 15:25:49 +0530

    add GCDorHCF.java

---
+++ 02_BasicMaths/GCDorHCF.java
@@ -0,0 +1,11 @@
+public class GCDorHCF {
+    public static int calcGCD(int n, int m){
+        int gcd = 1;
+        for(int i=1;i<= Math.min(n,m);i++){
+            if(n%i==0 && m%i==0){
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+}
