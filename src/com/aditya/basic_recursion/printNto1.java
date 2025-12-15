92357a04427ff13748260acc3e8da59c0be673fc
Aditya Saraswat <saraswataditya664@gmail.com>
2025-12-16 01:10:24 +0530

    add  printNto1.java

--- src/com/aditya/basic_recursion/printNto1.java
+++ src/com/aditya/basic_recursion/printNto1.java
@@ -1,4 +1,25 @@
 package com.aditya.basic_recursion;
 
+import java.util.Scanner;
+
+class funct{
+    void function(int i,int n){
+        if (i ==0 ){
+            return;
+        }
+        else{
+            System.out.println(i);
+            function(i-1,n);
+        }
+    }
+}
+
 public class printNto1 {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        funct obj = new funct();
+        obj.function(n,n);
+    }
 }
