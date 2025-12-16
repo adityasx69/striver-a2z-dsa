d16ae8fa3e6060934343a519b6c0553a8e56fbbf
Aditya Saraswat <saraswataditya664@gmail.com>
2025-12-16 15:55:11 +0530

    add factorial.java

--- src/com/aditya/basic_recursion/factorial.java
+++ src/com/aditya/basic_recursion/factorial.java
@@ -1,4 +1,25 @@
 package com.aditya.basic_recursion;
 
+import java.util.Scanner;
+
+class fact{
+    public int fact(int n){
+        if(n == 1){
+            return 1;
+        }
+        else{
+            return n * fact(n-1);
+        }
+    }
+}
+
 public class factorial {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        fact obj = new fact();
+        int sum = obj.fact(n);
+        System.out.println(sum);
+    }
 }
