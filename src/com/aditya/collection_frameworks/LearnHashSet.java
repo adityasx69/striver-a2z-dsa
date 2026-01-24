c7cd70d92b3c64489ec71f377311c8b0f3a8133a
Aditya Saraswat <saraswataditya664@gmail.com>
2026-01-25 00:22:11 +0530

    add LearnHashSet.java

--- src/com/aditya/collection_frameworks/LearnHashSet.java
+++ src/com/aditya/collection_frameworks/LearnHashSet.java
@@ -1,4 +1,32 @@
 package com.aditya.collection_frameworks;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LearnHashSet {
+    static void main() {
+        Set<Integer> set = new HashSet<>();
+
+        set.add(32);
+        set.add(2);
+        set.add(54);
+        set.add(21);
+        set.add(65);
+
+        System.out.println(set);
+
+        set.remove(54);
+
+        System.out.println(set);
+
+        System.out.println(set.contains(21));
+
+        System.out.println(set.isEmpty());
+
+        System.out.println(set.size());
+
+        set.clear();
+
+        System.out.println(set);
+    }
 }
