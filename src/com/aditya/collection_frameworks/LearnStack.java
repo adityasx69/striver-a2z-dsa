8f78c4ea3bc23a2bd2eac8054eac1f74f24c27eb
Aditya Saraswat <saraswataditya664@gmail.com>
2026-01-25 00:23:05 +0530

    add LearnStack.java

--- src/com/aditya/collection_frameworks/LearnStack.java
+++ src/com/aditya/collection_frameworks/LearnStack.java
@@ -1,4 +1,22 @@
 package com.aditya.collection_frameworks;
 
+import java.util.Stack;
+
 public class LearnStack {
+    static void main() {
+        Stack<String> animals = new Stack<>();
+
+        animals.push("Lion");
+        animals.push("Cat");
+        animals.push("Dog");
+        animals.push("Tiger");
+
+        System.out.println(animals);
+
+        System.out.println(animals.peek());
+
+        animals.pop();
+
+        System.out.println(animals);
+    }
 }
