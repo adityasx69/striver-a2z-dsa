6d4a40c59d327e44a48f36f4a30dcf6ab7c8875e
Aditya Saraswat <saraswataditya664@gmail.com>
2026-01-25 00:23:32 +0530

    add LearnTreeMap.java

--- src/com/aditya/collection_frameworks/LearnTreeMap.java
+++ src/com/aditya/collection_frameworks/LearnTreeMap.java
@@ -1,4 +1,40 @@
 package com.aditya.collection_frameworks;
 
+import java.util.*;
+
 public class LearnTreeMap {
+    static void main() {
+        Map<String, Integer> numbers = new TreeMap<>();
+
+        numbers.put("One",1);
+        numbers.put("Two",2);
+        numbers.put("Three",3);
+        numbers.put("Four",4);
+        numbers.put("Five",5);
+
+        numbers.putIfAbsent("Two",23);
+
+        System.out.println(numbers);
+
+        numbers.remove("Two");
+        System.out.println(numbers);
+
+        System.out.println(numbers.containsValue(4));
+        System.out.println(numbers.isEmpty());
+
+        for(Map.Entry<String, Integer> e : numbers.entrySet()){
+            System.out.println(e);
+
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        }
+
+        for (String keys : numbers.keySet()){
+            System.out.println(keys);
+        }
+
+        for (Integer value : numbers.values()){
+            System.out.println(value);
+        }
+    }
 }
