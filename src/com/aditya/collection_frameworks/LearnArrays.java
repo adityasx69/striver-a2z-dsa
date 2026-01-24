63c72efc51234a212a968624647d16880ee8b427
Aditya Saraswat <saraswataditya664@gmail.com>
2026-01-25 00:21:23 +0530

    add LearnArrays.java

--- src/com/aditya/collection_frameworks/LearnArrays.java
+++ src/com/aditya/collection_frameworks/LearnArrays.java
@@ -1,4 +1,21 @@
 package com.aditya.collection_frameworks;
 
+import java.util.Arrays;
+
 public class LearnArrays {
+    static void main() {
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+
+        int index = Arrays.binarySearch(numbers, 4);
+
+        System.out.println(index);
+        System.out.println();
+
+        int[] nums = {12,524,5,2,6,7,11,36,85,321};
+        Arrays.sort(nums);
+
+        for (int i : nums){
+            System.out.println(i);
+        }
+    }
 }
