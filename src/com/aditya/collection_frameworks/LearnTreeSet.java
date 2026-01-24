41ba7f7a5ecd42240a6959eef06ac152f96b7f36
Aditya Saraswat <saraswataditya664@gmail.com>
2026-01-25 00:23:19 +0530

    add LearnTreeSet.java

--- src/com/aditya/collection_frameworks/LearnTreeSet.java
+++ src/com/aditya/collection_frameworks/LearnTreeSet.java
@@ -1,4 +1,34 @@
 package com.aditya.collection_frameworks;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class LearnTreeSet {
+    static void main() {
+        Set<Integer> set = new TreeSet<>();
+
+        set.add(32);
+        set.add(2);
+        set.add(54);
+        set.add(21);
+        set.add(65);
+
+        System.out.println(set);
+
+        set.remove(54);
+
+        System.out.println(set);
+
+        System.out.println(set.contains(21));
+
+        System.out.println(set.isEmpty());
+
+        System.out.println(set.size());
+
+        set.clear();
+
+        System.out.println(set);
+    }
 }
