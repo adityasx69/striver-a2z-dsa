df50263862c2c510f5020162520abbad62e1e91d
Aditya Saraswat <saraswataditya664@gmail.com>
2026-01-25 00:22:33 +0530

    add LearnPriorityQueue.java

--- src/com/aditya/collection_frameworks/LearnPriorityQueue.java
+++ src/com/aditya/collection_frameworks/LearnPriorityQueue.java
@@ -1,4 +1,24 @@
 package com.aditya.collection_frameworks;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class LearnPriorityQueue {
+    static void main() {
+
+        Queue<Integer> pq = new PriorityQueue<>();
+
+        pq.offer(40);
+        pq.offer(9);
+        pq.offer(7);
+        pq.offer(90);
+
+        System.out.println(pq);
+        pq.poll();
+
+        System.out.println(pq);
+
+        System.out.println(pq.peek());
+
+    }
 }
