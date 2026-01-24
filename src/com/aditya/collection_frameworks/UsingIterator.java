30bb53b6e5ef3f898dd5fd4780b043d8531c1812
Aditya Saraswat <saraswataditya664@gmail.com>
2026-01-24 16:18:42 +0530

    add UsingIterator.java

---
+++ src/com/aditya/collection_frameworks/UsingIterator.java
@@ -0,0 +1,20 @@
+package com.aditya.collection_frameworks;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class UsingIterator {
+    static void main() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        Iterator<Integer> it = list.iterator();
+
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+}
