5f667fb42cc0f14562c222a0d99249021ddb1fd2
Aditya Saraswat <saraswataditya664@gmail.com>
2026-01-25 00:22:53 +0530

    add LearnQueue.java

--- src/com/aditya/collection_frameworks/LearnQueue.java
+++ src/com/aditya/collection_frameworks/LearnQueue.java
@@ -1,4 +1,22 @@
 package com.aditya.collection_frameworks;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LearnQueue {
+    static void main() {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(12);
+        queue.offer(24);
+        queue.offer(36);
+
+        System.out.println(queue);
+
+        System.out.println(queue.poll());
+
+        System.out.println(queue);
+
+        System.out.println(queue.peek());
+    }
 }
