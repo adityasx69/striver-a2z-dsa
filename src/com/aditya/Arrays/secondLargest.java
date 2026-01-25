212df0a8df6c492091c6519b2a7cb94f23dc4646
Aditya Saraswat <saraswataditya664@gmail.com>
2026-01-25 20:02:52 +0530

    add largestInArray.java

---
+++ src/com/aditya/Arrays/secondLargest.java
@@ -0,0 +1,45 @@
+//brute force
+//class secondLargest {
+//    public int secondLargest(int[] arr) {
+//        // code here
+//        Arrays.sort(arr);
+//        int n = arr.length;
+//        int largest = arr[n-1];
+//        int second_largest = -1;
+//        for (int i=n-2;i>=0;i--){
+//            if(arr[i]!=largest){
+//                second_largest =  arr[i];
+//                break;
+//            }
+//        }
+//        return second_largest;
+//    }
+//}
+//
+package com.aditya;
+
+class sLargest{
+    int sLargest(int[] arr, int n){
+        int largest = arr[0];
+        int secondLargest = -1;
+        for(int i=0;i<n;i++){
+            if(arr[i]>largest){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i]<largest && arr[i]>secondLargest){
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+}
+
+public class secondLargest {
+    static void main() {
+        int num = 5;
+        int[] nums = {1,3,6,5,3};
+            sLargest sl = new sLargest();
+            sl.sLargest(nums,num);
+    }
+}
---
+++ src/com/aditya/hashing/MostFrequent.java
@@ -0,0 +1,7 @@
+package com.aditya.hashing;
+
+public class leetcode_1838 {
+    static void main() {
+        
+    }
+}
