b13c822d256bf495539fd658ef164760fa997405
Aditya Saraswat <saraswataditya664@gmail.com>
2026-01-25 12:27:45 +0530

    add characterHashingAllChar.java

--- src/com/aditya/hashing/characterHashingAllChar.java
+++ src/com/aditya/hashing/characterHashingAllChar.java
@@ -1,4 +1,16 @@
 package com.aditya.hashing;
 
+import java.util.Scanner;
+
 public class characterHashingAllChar {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        int[] hash = new int[256];
+        for(int i=0; i<s.length();i++){
+            hash[s.charAt(i)]++;
+        }
+        char c = scanner.next().charAt(0);
+        System.out.println(hash[c]);
+    }
 }
