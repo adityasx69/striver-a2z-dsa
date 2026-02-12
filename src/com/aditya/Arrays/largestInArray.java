14975385c25af4a874a25ae9bff0d932b09ab4d1
Aditya Saraswat <saraswataditya664@gmail.com>
2026-02-12 22:23:47 +0530

    add largestInArray.java

--- src/com/aditya/Arrays/largestInArray.java
+++ src/com/aditya/Arrays/largestInArray.java
@@ -1,13 +1,21 @@
 class largestInArray {
     public static int largestInArray(int[] arr) {
         // code here
-        int max = arr[0];
-        for (int i=1;i<arr.length;i++){
-            if (arr[i] > max){
-                max = arr[i];
+        int largest = 0;
+        for(int i=0;i<arr.length - 1;i++){
+            if(arr[i+1] > arr[i]){
+                largest = i+1;
             }
         }
-        return max;
+        return arr[largest];
     }
 }
 
+// brute force
+//int max = arr[0];
+//        for (int i=1;i<arr.length;i++){
+//        if (arr[i] > max){
+//max = arr[i];
+//        }
+//        }
+//        return max;
