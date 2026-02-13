4000319d6f515660bc4d14c9ff92cd1fed34c74b
Aditya Saraswat <saraswataditya664@gmail.com>
2026-02-13 17:04:59 +0530

    add removeDuplicates.java

--- src/com/aditya/Arrays/removeDuplicates.java
+++ src/com/aditya/Arrays/removeDuplicates.java
@@ -1,4 +1,16 @@
 package com.aditya.Arrays;
 
-public class removeDuplicated {
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int unique = nums[0];
+        int k = 1;
+        for (int i=1;i<nums.length;i++){
+            if(nums[i] != unique){
+                nums[k] = nums[i];
+                unique = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
 }
