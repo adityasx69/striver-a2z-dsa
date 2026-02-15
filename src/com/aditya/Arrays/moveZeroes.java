2e9ebcfe10739b44edfbd629913b917d67c770b4
Aditya Saraswat <saraswataditya664@gmail.com>
2026-02-15 15:39:32 +0530

    add moveZeroes.java

--- src/com/aditya/Arrays/moveZeroes.java
+++ src/com/aditya/Arrays/moveZeroes.java
@@ -1,4 +1,41 @@
 package com.aditya.Arrays;
 
-public class moveZeroes {
-}
+//class Solution {
+//    public void moveZeroes(int[] nums) {
+//        int n = nums.length;
+//        int count = 0;
+//        int[] temp = new int[n];
+//        int index=0;
+//        for (int i=0;i<n;i++){
+//            if (nums[i] == 0 && n>1){
+//                temp[count] = nums[i];
+//                count++;
+//                index = i;
+//            }
+//            else if(nums[i]!=0 && n>1){
+//                nums[index] = nums[i];
+//            }
+//        }
+//
+//        for (int i=n-count;i<n;i++){
+//            nums[i] = temp[i];
+//        }
+//    }
+//}
+//
+
+
+class moveZeroes{
+        public void moveZeroes(int[] nums) {
+            int i = 0;
+
+            for(int j=0;j<nums.length;j++){
+                if (nums[j]!=0){
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                    i++;
+                }
+            }
+        }
+    }
