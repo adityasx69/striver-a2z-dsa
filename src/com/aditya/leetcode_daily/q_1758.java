961dec4c11bb91d843f67869b38716611ce70e31
Aditya Saraswat <saraswataditya664@gmail.com>
2026-03-05 16:07:45 +0530

    add q_1758.java

--- src/com/aditya/leetcode_daily/q_1758.java
+++ src/com/aditya/leetcode_daily/q_1758.java
@@ -1,4 +1,88 @@
-package com.aditya.leetcode_daily;
+// 1st way
 
-public class q_1758 {
+class Solution {
+    public int minOperations(String s) {
+        char[] arr = s.toCharArray();
+
+        int n = arr.length;
+        int count = 0;
+        for (int i=0;i<n-1;i++){
+            if((arr[i] == '0' && arr[i+1] == '1' && arr[i+2] == '0') || arr[i] == '1' && arr[i+1] == '0' && arr[i+2] == '1'){
+
+            }
+            else if (arr[i] == '0' && arr[i+1] == '0'){
+                arr[i+1] = '1';
+                count++;
+            }
+            else if(arr[i] == '1' && arr[i+1] == '1'){
+                arr[i+1] = '0';
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+// 2nd way
+
+class Solution {
+    public int minOperations(String s) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int count = 0;
+
+        for (int i=n;i>1;i--){
+            if ((arr[i] == '0' && arr[i-1] == '1') || (arr[i] == '1' && arr[i-1] == '0')){
+
+            }
+            else if(arr[i] == '0' && arr[i-1] == '0'){
+                arr[i] = '1';
+                count++;
+            }
+            else if(arr[i] == '1' && arr[i-1] == '1'){
+                arr[i] = '0';
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+// 3rd way
+
+class Solution {
+    public int minOperations(String s) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int count = 0;
+
+        for (int i=0;i<n-1;i++){
+            if (n>3 && (arr[i]=='1' && arr[i+1]=='0')){
+                arr[i+2] = '1';
+                count++;
+                i=i+2;
+            }
+            else if(n>3 && (arr[i]=='1' && arr[i+1]=='1')){
+                arr[i+1]='0';
+                arr[i+2]='1';
+                count++;
+                i=i+2;
+            }
+            else if(n>3 && (arr[i]=='0' && arr[i+1]=='0')){
+                arr[i+1]='1';
+                arr[i+2]='0';
+                count++;
+                i=i+2;
+            }
+            else if(count<3 && (arr[i]=='1' && arr[i+1]=='1')){
+                arr[i+1] = '0';
+                count++;
+            }
+            else if(count<3 && (arr[i]=='0' && arr[i+1]=='0')){
+                arr[i+1] = '1';
+                count++;
+            }
+        }
+        return count;
+    }
 }
