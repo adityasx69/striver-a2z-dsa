f938f59566096b8289d98db554b070ab81a4681f
Aditya Saraswat <saraswataditya664@gmail.com>
2026-05-14 21:06:17 +0530

    added basicSyntax

--- src/com/aditya/LinkedList/basicSyntax.java
+++ src/com/aditya/LinkedList/basicSyntax.java
@@ -17,7 +17,7 @@ class Node{
 
 public class basicSyntax {
     static void main() {
-        int[] arr = {2,5,6,8};
+        int[] arr = {2, 5, 6, 8};
         Node y = new Node(arr[3]);
         System.out.println(y.data);
     }
