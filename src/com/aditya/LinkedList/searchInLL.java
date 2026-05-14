16e09448fd3ae616365e9a3fef0e7cca6f4ab50f
Aditya Saraswat <saraswataditya664@gmail.com>
2026-05-14 22:04:46 +0530

    add searchInLL.java

--- src/com/aditya/LinkedList/searchInLL.java
+++ src/com/aditya/LinkedList/searchInLL.java
@@ -1,4 +1,13 @@
 package com.aditya.LinkedList;
 
 public class searchInLL {
+    public static boolean searchInLL(Node head, int val){
+
+    }
+    static void main() {
+        int[] arr = {12, 5, 6,  8};
+        Node head = arrayToLL.arrToLL(arr);
+        int val = 3;
+        System.out.println(searchInLL(head,val));
+    }
 }
